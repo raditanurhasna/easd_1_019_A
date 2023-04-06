aa8d78133624e28908292c088b60e8d262f74203
raditanurhasna <raditanurhasna1410@gmail.com>
2023-04-06 10:57:43 +0700

    Add project files.

---
+++ Exercise Algoritma/Exercise Algoritma.cpp
@@ -0,0 +1,46 @@
+// 1. Karena dapat mengatasi masalah data yang kita simpan 
+// 2. Struktur Algoritma ialah static dan Dynamic 
+// 3. Faktor Yang mempengaruhi efisiensi program ialah speed of machine (kecepatan profesor), compiler (penerjemah dari bahasa program ke informasi yang tepat)
+	// Operating System (Prmptograman Multi Platfom), Programming Langguange (Bahasa Yang digunakan), Site of the put (Ukuran dari Input)
+// 4. Quick Sort dikarenakan algoritma ini paling efisien didasarkan pada pendekatan dan menaklukan secara berturut-turut membagi masalah menjadi bagian-bagian
+	// yang lebih kecil hingga masalah menjadi sangat kecil.
+// 5. Quadratic = Bubble sort, Selection sort, Insertion sort ,shell sort  Longlinear = Merge sort, quick sort 
+
+
+#include <iostream>
+using namespace std;		
+
+int dita[39];					
+int n;
+
+void mergesort(int low, int high)
+{
+	int pivot, i, ra;
+	if (low > high)
+		return;
+	pivot = dita [low];
+	i = low + 1;
+	ra = high;
+	while (i <= ra)
+	{
+		while ((dita[ra] <= pivot) && (i <= high))
+		{
+			i++		
+		}
+	}
+	if (low < j)
+	{
+		
+	}
+}
+			
+
+
+	
+
+
+
+
+
+	
+
